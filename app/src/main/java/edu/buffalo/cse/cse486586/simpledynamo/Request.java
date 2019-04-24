9129c4b39e734d23e299dc87a084bb9420633243
Aashish Jain <aashish_jain@live.com>
2019-04-24 12:35:02 -0400

    Added base code - 1

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/Request.java
@@ -0,0 +1,76 @@
+package edu.buffalo.cse.cse486586.simpledynamo;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+enum RequestType {
+    JOIN, QUERY, QUERY_ALL, INSERT, DELETE, DELETE_ALL, QUIT, UPDATE_SUCCESSOR, UPDATE_PREDECESSOR;
+}
+
+public class Request {
+    static final String TAG = "REQUEST";
+    private static final String seperator = ",";
+
+    private int senderId;
+    private String hashedSenderId;
+    private RequestType requestType;
+    private String key, value;
+
+    Request(int senderId, RequestType requestType) {
+        this.senderId = senderId;
+        this.hashedSenderId = SimpleDynamoProvider.generateHash(Integer.toString(senderId));
+        this.requestType = requestType;
+        this.key = null;
+        this.value = null;
+    }
+
+    Request(int senderId, String key, String value, RequestType requestType) {
+        this.senderId = senderId;
+        this.hashedSenderId = SimpleDynamoProvider.generateHash(Integer.toString(senderId));
+        this.requestType = requestType;
+        this.key = key;
+        this.value = value;
+    }
+
+    /* To parse from the string */
+    Request(String string) throws IOException {
+        String[] strings = string.split(this.seperator);
+        if (strings.length == 5) {
+            this.requestType = RequestType.valueOf(strings[0]);
+            this.senderId = Integer.parseInt(strings[1]);
+            this.hashedSenderId = strings[2];
+            this.key = strings[3];
+            this.value = strings[4];
+        } else {
+            Log.d(TAG, string + " " + strings.length);
+            throw new IOException("Unable to parse the String");
+        }
+    }
+
+    public RequestType getRequestType() {
+        return requestType;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getSenderId() {
+        return this.senderId;
+    }
+
+    public String getHashedSenderId() {
+        return this.hashedSenderId;
+    }
+
+    @Override
+    public String toString() {
+        return requestType + seperator + senderId + seperator + hashedSenderId + seperator +
+                key + seperator + value;
+    }
+}
--- app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
@@ -1,60 +1,66 @@
 package edu.buffalo.cse.cse486586.simpledynamo;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Formatter;
-
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 
 public class SimpleDynamoProvider extends ContentProvider {
 
-	@Override
-	public int delete(Uri uri, String selection, String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
-	@Override
-	public String getType(Uri uri) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public Uri insert(Uri uri, ContentValues values) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getType(Uri uri) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public boolean onCreate() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Cursor query(Uri uri, String[] projection, String selection,
-			String[] selectionArgs, String sortOrder) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean onCreate() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public int update(Uri uri, ContentValues values, String selection,
-			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-    private String genHash(String input) throws NoSuchAlgorithmException {
-        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-        byte[] sha1Hash = sha1.digest(input.getBytes());
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    static String generateHash(String input) {
         Formatter formatter = new Formatter();
-        for (byte b : sha1Hash) {
-            formatter.format("%02x", b);
+        try {
+            MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+            byte[] sha1Hash = sha1.digest(input.getBytes());
+            for (byte b : sha1Hash) {
+                formatter.format("%02x", b);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("HASHER", "SHA-1 not found for encryption");
         }
         return formatter.toString();
     }
