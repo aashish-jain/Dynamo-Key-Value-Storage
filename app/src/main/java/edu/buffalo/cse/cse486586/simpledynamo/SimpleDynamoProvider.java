2de932a81091e505d46868b732a424a513d9adb3
Aashish Jain <aashish_jain@live.com>
2019-04-24 12:46:14 -0400

    Added base code - 2

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
@@ -2,8 +2,12 @@ package edu.buffalo.cse.cse486586.simpledynamo;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import java.security.MessageDigest;
@@ -11,44 +15,24 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 
 public class SimpleDynamoProvider extends ContentProvider {
+    KeyValueStorageDBHelper dbHelper;
+    SQLiteDatabase dbWriter, dbReader;
+    static final int selfProcessIdLen = 4;
+    int myID;
+    String myHash;
+    static final int serverId = 5554;
+    //    TreeMap<String, Client> chordRingMap;
+    static final String DELETE_TAG = "DELETE_TAG", CREATE_TAG = "CREATE_TAG",
+            INSERT_TAG = "INSERT_TAG", QUERY_TAG = "QUERY_TAG", FETCH_TAG = "FETCH";
 
-
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+    static final String[] projection = new String[]{
+            KeyValueStorageContract.KeyValueEntry.COLUMN_KEY,
+            KeyValueStorageContract.KeyValueEntry.COLUMN_VALUE
+    };
 
     @Override
     public String getType(Uri uri) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public boolean onCreate() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public int update(Uri uri, ContentValues values, String selection,
-                      String[] selectionArgs) {
-        // TODO Auto-generated method stub
-        return 0;
+        return "string";
     }
 
     static String generateHash(String input) {
@@ -64,4 +48,87 @@ public class SimpleDynamoProvider extends ContentProvider {
         }
         return formatter.toString();
     }
+
+    /* https://stackoverflow.com/questions/3105080/output-values-found-in-cursor-to-logcat-android */
+    private static String cursorToString(Cursor cursor) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int cursorCount = 1;
+        if (cursor.moveToFirst()) {
+            do {
+                int columnsQty = cursor.getColumnCount();
+                for (int idx = 0; idx < columnsQty; ++idx) {
+                    stringBuilder.append(cursor.getString(idx));
+                    if (idx < columnsQty - 1)
+                        stringBuilder.append(",");
+                }
+                if (cursorCount < cursor.getCount())
+                    stringBuilder.append("\n");
+                cursorCount++;
+            } while (cursor.moveToNext());
+        }
+        return stringBuilder.toString();
+    }
+
+    private static Cursor cursorFromString(String queryResult) {
+        MatrixCursor matrixCursor = new MatrixCursor(new String[]{"key", "value"});
+        String key, value;
+        String[] splitValues;
+        for (String row : queryResult.split("\n")) {
+            splitValues = row.split(",");
+            try {
+                key = splitValues[0];
+                value = splitValues[1];
+                matrixCursor.addRow(new String[]{key, value});
+            } catch (Exception e) {
+
+            }
+        }
+        return matrixCursor;
+    }
+
+    private int getProcessId() {
+        /* https://stackoverflow.com/questions/10115533/how-to-getsystemservice-within-a-contentprovider-in-android */
+        String telephoneNumber =
+                ((TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE)).getLine1Number();
+        int length = telephoneNumber.length();
+        telephoneNumber = telephoneNumber.substring(length - selfProcessIdLen);
+        int id = Integer.parseInt(telephoneNumber);
+        return id;
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
 }
