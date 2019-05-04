544257d199cffe258273301caa9fb396d7fd8d83
Aashish Jain <aashish_jain@live.com>
2019-05-04 00:32:29 -0400

    JAC

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/Client.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/Client.java
@@ -1,6 +1,5 @@
 package edu.buffalo.cse.cse486586.simpledynamo;
 
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
--- app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/Request.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/Request.java
@@ -76,7 +76,7 @@ public class Request {
                 key + seperator + value;
     }
 
-    public int getSender(){
+    public int getSender() {
         return senderId;
     }
 }
